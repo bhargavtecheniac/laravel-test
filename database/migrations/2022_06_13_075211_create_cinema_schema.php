701a11fc39154edfcdedd1fb483e84b3c0785580
Bhargav Chudasama <bhargav@techeniac.com>
2022-09-19 14:27:08 +0530

    laravel test

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -7,37 +7,62 @@ use Illuminate\Support\Facades\Schema;
 class CreateCinemaSchema extends Migration
 {
     /**
-    # Create a migration that creates all tables for the following user stories
+        # Create a migration that creates all tables for the following user stories
 
-    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-    To not introduce additional complexity, please consider only one cinema.
+        For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
+        To not introduce additional complexity, please consider only one cinema.
 
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+        Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
 
-    ## User Stories
+        ## User Stories
 
-     **Movie exploration**
-     * As a user I want to see which films can be watched and at what times
-     * As a user I want to only see the shows which are not booked out
+        **Movie exploration**
+        * As a user I want to see which films can be watched and at what times
+        * As a user I want to only see the shows which are not booked out
 
-     **Show administration**
-     * As a cinema owner I want to run different films at different times
-     * As a cinema owner I want to run multiple films at the same time in different locations
+        **Show administration**
+        * As a cinema owner I want to run different films at different times
+        * As a cinema owner I want to run multiple films at the same time in different locations
 
-     **Pricing**
-     * As a cinema owner I want to get paid differently per show
-     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
+        **Pricing**
+        * As a cinema owner I want to get paid differently per show
+        * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
 
-     **Seating**
-     * As a user I want to book a seat
-     * As a user I want to book a vip seat/couple seat/super vip/whatever
-     * As a user I want to see which seats are still available
-     * As a user I want to know where I'm sitting on my ticket
-     * As a cinema owner I dont want to configure the seating for every show
+        **Seating**
+        * As a user I want to book a seat
+        * As a user I want to book a vip seat/couple seat/super vip/whatever
+        * As a user I want to see which seats are still available
+        * As a user I want to know where I'm sitting on my ticket
+        * As a cinema owner I dont want to configure the seating for every show
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('cinema', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->integer('cinema_id')->unsigned();
+            $table->string('name');
+            $table->double('price')->default(0);
+            $table->integer('vip_seats')->default(0);
+            $table->foreign('cinema_id')->references('id')->on('cinema')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('movies_time', function($table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('seat_number');
+            $table->integer('movies_id')->unsigned()->nullable();
+            $table->foreign('movies_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
